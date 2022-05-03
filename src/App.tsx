8c2b147091cd2bdb5f191edbee441aa9443112bc
Yari Taft <yari.taft@gmail.com>
2022-05-03 17:23:07 -0300

    Removed warning.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Login } from "./components/Login/Login";
 import { getToken } from "./actions/Login/login";
